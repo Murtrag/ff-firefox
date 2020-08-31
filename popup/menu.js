4e26d9a0e710e6e4b0147668887c7199750f5ec8
murtrag <lukasz.czernik@gmail.com>
2020-08-31 18:24:44 +0200

    Created dropdown menu to navbar icon, manager window, closing button to manager

--- popup/menu.js
+++ popup/menu.js
@@ -6,9 +6,13 @@ document.addEventListener("click", function(e) {
 		  active: true,
 		  currentWindow: true
 	})
+	var page_id = e.target.dataset.id;
 	tab.then(e=>{
 		console.log(e[0].id);
-		browser.tabs.sendMessage(e[0].id, { type: "open_menu" });
+		browser.tabs.sendMessage(e[0].id, {
+			type: "open_menu",
+			page: page_id
+		});
 	});
 
 	// browser.tabs.getCurrent().then((v)=>{console.log(v)})
